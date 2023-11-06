c4a254f6457bf65f9bbe77e094d46bd8260715ec
Lucas Giraldi <lucas.favaro@freehub.com.br>
2023-11-06 20:40:28 -0300

    WIP

--- SmartLock/SmartLock/Views/Login.xaml.cs
+++ SmartLock/SmartLock/Views/Login.xaml.cs
@@ -24,7 +24,7 @@ namespace SmartLock
         private async void ButtonClickedAsync(object sender, EventArgs e)
         {
 
-            var s3 = new AmazonS3Client("ASIA2IVKB6WV5W6BH36V", "Hra2l6IN0CfssdlUlwB2HXOrVfjqJPrVZn/osyVr", "FwoGZXIvYXdzEOD//////////wEaDLVkOOCfaMG5HeHKUiK+AaJQ+iHmRfBC8Raikr0jNKrdEvRwu6/Xj97SfW168Rjc6Q5V4UKgaqsuQQHf45HXhVBowfM0NCZHs2HmGGCq+ID/Xo0nhSlHu2pHjap56ck7vdjtb0CdQ3awGgA5Nx57WkpKQFUGdmnGTjbKifHNBMb1viGkl40KuAm5k3N43Enre9qzmvfSHRR3KTmpPjw/poy8w2hVt6qhg+3FKi6AQNPXcEBwNxrmi6gYUPEuLw6PMzghbCyxjmFHLv5+eyYosIaGqgYyLfpyZHIJjB4reUyn0PI0h9IjFZDum7UuFmU9zR+YhnWIU6y6TXxzvVRLDAeWdQ==", Amazon.RegionEndpoint.USEast1);
+            var s3 = new AmazonS3Client("ASIA2IVKB6WVVQYDL7VB", "dSeg+cct93S9YlQuGaaqgV/qwECKkg/46rVRDkKW", "FwoGZXIvYXdzEHEaDHqZejwrmc6+ekIUliK+AYrw3DTYyz7Dp60dmPGsz0TEmP2nm9bKKg/MYQjQ0DlJvvJLSI51U7HRN5bXPTEQ2MOEHffUiUsLQBf6mGTJv67bpdHbaduRcCmVk7ETTYnDE5FhZgjokGhjT6Z2Z1CFeYJ4GINHaXUf7JkbqiCCKLReARpSZXMbXc1ysecqMPZFPbUtdV4G/if2nzK6gFvWCdMMyEnJWKCUzFG8/oAdH3X+EXwedp2BZPnFMK1oLl5Bm2p4KhIAx+7tZwIWVU4o7+elqgYyLUELLv+aB2FrfIQ8GB3Ns14hhsE0qxykyz/AiaB/SWVu58q7OfNexFPCBP0M0g==", Amazon.RegionEndpoint.USEast1);
 
             var fileTransferUtil = new TransferUtility(s3);
             string dbName = "dbSmartLock.db3";
@@ -33,20 +33,20 @@ namespace SmartLock
 
             var fileTransferUtilityRequest = new TransferUtilityUploadRequest
             {
-                BucketName = "xamarian-smart-lock",
+                BucketName = "bucket-smart-lock-senai",
                 FilePath = filePath,
                 StorageClass = S3StorageClass.StandardInfrequentAccess,
                 PartSize = 6291456,
-                Key = "teste-imagem",
+                Key = $"BKP- {login.Text}-{DateTime.Now}",
                 CannedACL = S3CannedACL.PublicRead
             };
 
-            await fileTransferUtil.UploadAsync(fileTransferUtilityRequest);
 
             ServiceDBUser database = new ServiceDBUser(App.DbPath);
             if (database.AutorizarLogin(login.Text, senha.Text))
             {
                 await Navigation.PushAsync(new Home());
+                await fileTransferUtil.UploadAsync(fileTransferUtilityRequest);
             }
             else
             {
